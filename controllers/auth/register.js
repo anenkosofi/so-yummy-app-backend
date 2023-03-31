065395c8eea8f9624eebd837b89fac47ed8c73f0
ValeriiaM23 <mezencevavalerie@gmail.com>
2023-03-31 23:51:03 +0200

    adding auth routes

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,16 +1,27 @@
+const bcrypt = require("bcryptjs");
 const { User } = require("../../models");
+const { Conflict } = require("http-errors");
+
 
 const register = async (req, res) => {
     const body = req.body;
-    // const { email } = body;
-    // const user = await User.findOne({ email });
-    const newUser = await User.create(body);
+    const { name, email, password } = body;
+    const user = await User.findOne({ email });
+    if (user) {
+        throw new Conflict(`User with this email: ${email} already exists`);
+    }
+
+    const hashPassword = bcrypt.hashSync(password, 10);
+    const newUser = await User.create({ name, email, password: hashPassword });
+  
 
     res.status(201).json({
+        status: 'success',
         user: {
-            newUser,
+            name: newUser.name,
+            email: newUser.email,
         },
     });
-};
+}; 
 
 module.exports = register; 
