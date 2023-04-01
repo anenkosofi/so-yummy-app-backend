182f06195c74ad8c0a3b3aaed38f60d72ff6494a
YuliiaMakhovska <y.makhovska@gmail.com>
2023-04-01 15:41:26 +0300

    fix getByCategory

--- controllers/recipes/commonRecipes/getRecipesByCategory.js
+++ controllers/recipes/commonRecipes/getRecipesByCategory.js
@@ -1,24 +1,19 @@
-const CommonRecipe = require("../../models");
+const { CommonRecipe } = require("../../../models");
+const { HttpError } = require("../../../helpers");
 
 const getRecipesByCategory = async (req, res) => {
   const { category } = req.params;
-  const { query } = req.query;
   const { page = 1, limit = 8 } = req.query;
   const skip = (page - 1) * limit;
-  const recipes = await CommonRecipe.find(
-    {
-      category,
-      title: { $regex: new RegExp(query, "i") },
-    },
-    "-createdAt -updatedAt",
-    { skip, limit }
-  );
-
+  const filters = { category: { $regex: new RegExp(category, "i") } };
+  const recipes = await CommonRecipe.find(filters, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  });
   res.json({
     status: "success",
     code: 200,
     recipes,
   });
 };
-
 module.exports = getRecipesByCategory;
