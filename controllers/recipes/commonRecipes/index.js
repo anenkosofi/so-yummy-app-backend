406c7b17de8cc18bd91075ad7b8ecccc884f5740
AnastasiiaSteblyna <98827282+AnastasiiaSteblyna@users.noreply.github.com>
2023-04-01 14:09:13 +0300

    закомічу поки не поламалося.

--- controllers/recipes/commonRecipes/index.js
+++ controllers/recipes/commonRecipes/index.js
@@ -2,8 +2,12 @@ const { ctrl } = require("../../../helpers");
 
 const getRecipesByCategory = require("./getRecipesByCategory");
 const getCategoryRecipes = require("./getCategoryRecipes");
+const getRecipesByIngredients = require("./getRecipesByIngredients");
+const getRecipesByTitle = require("./getRecipesByTitle");
 
 module.exports = {
   getRecipesByCategory: ctrl(getRecipesByCategory),
   getCategoryRecipes: ctrl(getCategoryRecipes),
+  getRecipesByTitle: ctrl(getRecipesByTitle),
+  getRecipesByIngredients: ctrl(getRecipesByIngredients),
 };
--- routes/api/commonRecipes.js
+++ routes/api/commonRecipes.js
@@ -3,6 +3,8 @@ const express = require("express");
 const { commonRecipes } = require("../../controllers");
 const { categories } = require("../../controllers");
 
+const { authenticate, validateId } = require("../../middlewares");
+
 const router = express.Router();
 
 router.get("/category-list", categories.getCategoriesList);
@@ -10,5 +12,13 @@ router.get("/category-list", categories.getCategoriesList);
 router.get("/main-page", commonRecipes.getCategoryRecipes);
 
 router.get("/:category", commonRecipes.getRecipesByCategory);
+
+router.get("/title/:query", authenticate, commonRecipes.getRecipesByTitle);
+
+router.get(
+  "/ingredient/:query",
+  authenticate,
+  commonRecipes.getRecipesByIngredients
+);
 
 module.exports = router;
