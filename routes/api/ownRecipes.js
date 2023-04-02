6b7857b43cf9d2c8792f34c616525c3c32c9968d
Kyrylo Dyachenko <104459315+kirill25460@users.noreply.github.com>
2023-04-02 21:56:28 +0300

    start

--- routes/api/ownRecipes.js
+++ routes/api/ownRecipes.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const { ownRecipes } = require("../../controllers");
+const { authenticate } = require("../../middlewares");
+const { validation } = require("../../middlewares");
+
 
 const router = express.Router();
 
-router.get("/", async(req,res)=> {
-    
-})
+router.get("/", authenticate,ownRecipes.getAllRecipes);
+
+router.post(
+    "/",
+    authenticate,
+    validation(),
+    ownRecipes.addRecipe
+  );
+
+
+router.delete("/:id", authenticate, ownRecipes.deleteRecipe);
 
 module.exports = router;
