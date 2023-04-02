be163eb6019095769f8e1a6e10670c33bb1608b3
AleksandrTurlakov <aturlakov@ukr.net>
2023-04-02 18:15:18 +0300

    add swagger, first and point

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
@@ -15,6 +17,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/users", authRouter);
 app.use("/api/ingredients", ingredientsRouter);
