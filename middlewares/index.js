7d7e510a62200fba2b5ba9909e491b4bc598f10b
Sofiia Anenko <anenko.sofiia@gmail.com>
2023-04-02 17:09:28 +0300

    fix: add env

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,19 +1,13 @@
 const validation = require("./validateBody");
 const authenticate = require("./authenticate");
-const passport = require("./googleAuthenticate"); 
-const uploadCloud = require("./uploadCloud");
-
-
-module.exports = {
-    validation, 
-    authenticate,
-    passport,
-    uploadCloud, 
-}
 const validateId = require("./validateId");
+const passport = require("./googleAuthenticate");
+const uploadCloud = require("./uploadCloud");
 
 module.exports = {
   validation,
   authenticate,
   validateId,
+  passport,
+  uploadCloud,
 };
