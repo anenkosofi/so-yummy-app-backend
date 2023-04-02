ceee932d82db309aaef29c6731899bfa9d1e8b6b
Sofiia Anenko <anenko.sofiia@gmail.com>
2023-04-02 16:57:24 +0300

    refactor: fix identifiers

--- models/index.js
+++ models/index.js
@@ -1,30 +1,22 @@
-
-const {User, joiRegisterSchema, joiLoginSchema, refreshSchema, updateUserSchema} = require("./user");
-const CommonRecipe = require("./commonRecipe");
-
-const { User, joiRegisterSchema, joiLoginSchema } = require("./user");
+const {
+  User,
+  joiRegisterSchema,
+  joiLoginSchema,
+  refreshSchema,
+  updateUserSchema,
+} = require("./user");
 const { CommonRecipe, addFavoriteSchema } = require("./commonRecipe");
 const OwnRecipe = require("./ownRecipe");
 const Ingredient = require("./ingredient");
 
 module.exports = {
-
-    User,
-    joiRegisterSchema,
-    joiLoginSchema,
-    refreshSchema,
-    updateUserSchema,
-    CommonRecipe,
-    OwnRecipe,
-    Ingredient,
-}; 
-
   User,
   joiRegisterSchema,
   joiLoginSchema,
+  refreshSchema,
+  updateUserSchema,
   CommonRecipe,
   OwnRecipe,
   Ingredient,
   addFavoriteSchema,
 };
-
