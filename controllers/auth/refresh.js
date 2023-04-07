577aa0f9f5ef7262abf814a72c6fc625e47f8966
ValeriiaM23 <mezencevavalerie@gmail.com>
2023-04-07 12:01:38 +0200

    fixing login and refresh

--- controllers/auth/refresh.js
+++ controllers/auth/refresh.js
@@ -6,20 +6,19 @@ const { ACCESS_SECRET_KEY, REFRESH_SECRET_KEY } = process.env;
 
 const refresh = async (req, res, next) => {
   const { refreshToken: token } = req.body;
+  let tokens = {};
+  let userId = '';
   try {
     const { id } = jwt.verify(token, REFRESH_SECRET_KEY);
     const user = await User.findById(id);
     if (user.refreshToken !== token) {
       throw new BadRequest("Your token is invalid");
     }
-    // const isValid = await User.findOne({ refreshToken: token });
-    // if (!isValid) {
-    //     throw new Forbidden("Your token is invalid")
-    // }
 
     const payload = {
       id,
     };
+
     const accessToken = jwt.sign(payload, ACCESS_SECRET_KEY, {
       expiresIn: "2h",
     });
@@ -27,13 +26,20 @@ const refresh = async (req, res, next) => {
       expiresIn: "7d",
     });
 
-    res.status(201).json({
-      accessToken,
+    userId = id; 
+    tokens = {
+      accessToken, 
       refreshToken,
-    });
+    }
+    
   } catch (error) {
     throw new Forbidden("Access Denied. Please try logging in again");
   }
-};
+  await User.findByIdAndUpdate(userId, { ...tokens });
+  
+    res.status(201).json({
+     ...tokens
+    });
+}; 
 
 module.exports = refresh;
--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -6,14 +6,14 @@ const {ACCESS_SECRET_KEY } = process.env;
 
 const authenticate = async (req, res, next) => {
     const { authorization = "" } = req.headers;
-    const [bearer, token] = authorization.split(" ");
+    const [bearer, accessToken] = authorization.split(" ");
     try {
-        if (!token || bearer !== "Bearer") {
+        if (!accessToken || bearer !== "Bearer") {
             throw new Unauthorized("User is not authorized");
         }
-          const { id } = jwt.verify(token, ACCESS_SECRET_KEY);
+          const { id } = jwt.verify(accessToken, ACCESS_SECRET_KEY);
         const user = await User.findById(id);
-        if (!user || !user.accessToken) {
+        if (!user || !user.accessToken || user.accessToken !== accessToken) {
             throw new Unauthorized("User is not authorized");
         }
         
