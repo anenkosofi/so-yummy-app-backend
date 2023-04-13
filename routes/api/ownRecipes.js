6bce490cbd5f98dd43a54eec5464cf6fec3a6a2d
ValeriiaM23 <mezencevavalerie@gmail.com>
2023-04-13 23:04:30 +0200

    adding ass recipe controller

--- routes/api/ownRecipes.js
+++ routes/api/ownRecipes.js
@@ -8,6 +8,8 @@ const { addOwnSchema } = require("../../models/ownRecipe");
 
 router.get("/", authenticate, controllers.getAllRecipes);
 
+router.post("/", authenticate, upload.single("image"),  validation(addOwnSchema), controllers.addRecipe);
+
 router.get("/:id", authenticate, controllers.getRecipeById);
 
 router.post("/", authenticate, validation(addOwnSchema), controllers.addRecipe);
